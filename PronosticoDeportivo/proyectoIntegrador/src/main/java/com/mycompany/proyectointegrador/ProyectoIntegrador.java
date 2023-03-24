bf29d96e37b036440ea6dab12057f7f79a366707
pbarzaghi <pablobarzaghi@gmail.com>
2023-03-23 22:39:48 -0300

    probrando netbean ARCHIVOS PARA LEER 3 RESULTADOS Y PRONOSTICO

--- PronosticoDeportivo/proyectoIntegrador/src/main/java/com/mycompany/proyectointegrador/ProyectoIntegrador.java
+++ PronosticoDeportivo/proyectoIntegrador/src/main/java/com/mycompany/proyectointegrador/ProyectoIntegrador.java
@@ -5,6 +5,7 @@
 package com.mycompany.proyectointegrador;
 
 import java.util.List;
+import javax.swing.JOptionPane;
 import tpi.ar.programa.lectura.file.FileCvs;
 import tpi.ar.programa.pronostico.GanadorPronostico;
 import tpi.ar.programa.pronostico.Pronostico;
@@ -17,12 +18,57 @@ import tpi.ar.programa.pronostico.Pronostico;
 public class ProyectoIntegrador {
 
     public static void main(String[] args) {
-    
-        //TODO aca hay que leer los archivos por parametros no como esta ahora
+        String csvResultado="";         
+        String csvPronostico = ""; 
+        boolean salida=true;
+        String strNrofileResultado;
+        do{
+         strNrofileResultado= JOptionPane.showInputDialog(null,"Elija el archivo: 1 - 2 - 3","ARCHIVO RESULTADO", 3);
+            switch(strNrofileResultado) {
+                case "1":
+                    csvResultado = "src\\main\\resources\\resultado1.csv";
+                    salida=false;
+                    break;
+                  case "2":
+                     csvResultado = "src\\main\\resources\\resultado2.csv";  
+                     salida=false;
+                    break; 
+                   case "3":
+                     csvResultado = "src\\main\\resources\\resultado3.csv";
+                     salida=false;
+                    break;   
+                default:
+                     strNrofileResultado= JOptionPane.showInputDialog(null,"Elija el archivo: 1 - 2 - 3","ARCHIVO RESULTADO", 3);
         
-
-        String csvResultado = "src\\main\\resources\\resultado.csv";         
-        String csvPronostico = "src\\main\\resources\\pronostico.csv"; 
+            }
+        
+        }while(salida);
+        
+        
+                
+         salida=true;
+         String strNrofilePronostico;
+        do{
+            strNrofilePronostico= JOptionPane.showInputDialog(null,"Elija el archivo: 1 - 2 - 3)","ARCHIVO PRONOSTICO ",3);
+             
+            switch( strNrofilePronostico) {
+                case "1":
+                     csvPronostico = "src\\main\\resources\\pronostico1.csv"; 
+                    salida=false;
+                    break;
+                  case "2":
+                      csvPronostico = "src\\main\\resources\\pronostico2.csv";   
+                     salida=false;
+                    break; 
+                   case "3":
+                      csvPronostico = "src\\main\\resources\\pronostico3.csv"; 
+                     salida=false;
+                    break;   
+                default:
+                       strNrofilePronostico= JOptionPane.showInputDialog(null,"Elija el archivo: 1 - 2 - 3)","ARCHIVO PRONOSTICO ",4);
+             
+            }
+              }while(salida);   
      
     
        FileCvs file= new FileCvs();
@@ -30,9 +76,8 @@ public class ProyectoIntegrador {
    //   List<Ronda> listaRonda= file.leerArchivoResultado(csvResultdo);
       List<Pronostico> listaPronostico= file.leerArchivoPronostico(csvResultado,csvPronostico);
         // Invocar los metodos para obtebner los resulados una ves cargados los objetos
-    
-        GanadorPronostico ganador= new GanadorPronostico();
-        System.out.println( ganador.ganadorDelPronostico(listaPronostico));
+    GanadorPronostico ganador= new GanadorPronostico();
+    System.out.println( ganador.ganadorDelPronostico(listaPronostico));
          
 
       
